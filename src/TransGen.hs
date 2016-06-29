3291b9e871e74aa2fcaccd40fd94e0d39c8ccae1
Kyriakos Katsamaktsis <kyriakosk5@gmail.com>
2016-06-29 19:06:27 +0100

    Getting general information from front end ast
    TransGen has functions for getting general information from the frontend
    ast and transforming it to the appropriate backend constructs.
    So far it has functions for extracting names of machines, channels,
    networks and machine sizes.

---
+++ src/TransGen.hs
@@ -0,0 +1,75 @@
+-----------------------------------------------------------------
+---------- Functions that extract essential information ---------
+-------------------- from the front-end AST ---------------------
+
+module TransGen where
+
+-----------------------------------------------------------------
+
+import qualified Ast as F
+import qualified Backend.Murphi.MurphiAST as B
+
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+
+-- Helper functions
+
+--------------------------------
+
+-- is a net ordered?
+isOrdered :: F.Network -> Bool
+isOrdered (F.Network F.Ord _ _) = True
+isOrdered _               = False
+
+--------------------------------
+
+
+----------------------------------------------------------------
+----------------------------------------------------------------
+
+-- Getting names and sizes (non machine specific)
+
+--------------------------------
+
+getMachineNames :: F.Ast -> [B.MachineType]
+getMachineNames frontAST = let machinesAllInfo = F.machines frontAST
+                           in  map F.machineType machinesAllInfo
+
+--------------------------------
+
+getMachineSizes :: F.Ast -> [B.Size]
+getMachineSizes frontAST = let machinesAllInfo = F.machines frontAST
+                           in  map F.size machinesAllInfo
+
+--------------------------------
+
+getVCNames :: F.Ast -> [B.VCName]
+getVCNames frontAST = let channelsAllInfo = F.channels frontAST
+                      in  map (\ (F.Channel name) -> name) channelsAllInfo
+
+--------------------------------
+
+getAllNetNames :: F.Ast -> [B.NetName]
+getAllNetNames frontAST = let nets = F.networks frontAST
+                          in  map ( \(F.Network _ name _) -> name ) nets
+
+--------------------------------
+
+getOrdNetNames :: F.Ast -> [B.OrderedNetName]
+getOrdNetNames frontAST = let allNets = F.networks frontAST
+                              onlyOrdered = filter isOrdered allNets
+                          in  map( \(F.Network _ name _) -> name ) onlyOrdered
+
+--------------------------------
+
+getUnordNetNames :: F.Ast -> [B.UnorderedNetName]
+getUnordNetNames frontAST = let allNets = F.networks frontAST
+                                onlyUnordered = filter (not . isOrdered) allNets
+                            in  map( \(F.Network _ name _) -> name ) onlyUnordered
+
+--------------------------------
+
+
+
+----------------------------------------------------------------
+----------------------------------------------------------------
