31e48b2a57735d45e4faa4720ab12365fbde9077
Kyriakos Katsamaktsis <kyriakosk5@gmail.com>
2016-07-20 23:22:08 +0300

    Methods for getting guards in TransGuard
    This file has methods for transforming guards checking the message being
    received and for getting a guard datatype in the backend from a state at
    the frontend. The latter is useful in machine functions and self issued
    rules

---
+++ src/TransGuard.hs
@@ -0,0 +1,78 @@
+---------------------------- Guards -----------------------------
+
+module TransGuard where
+
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+
+import qualified Ast as F
+import qualified Backend.Murphi.MurphiAST as B
+import Data.Maybe -- for fromJust
+import TransGen
+import TransMsg
+
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+
+transReceiveMsg :: F.MachineType -> [F.Field]  -- the machine and its fields
+                   -> [B.MsgArg]               -- standard form of msg in Murphi
+                   -> B.LocalVariables
+                   -> F.Guard -> B.Guard
+transReceiveMsg machine machineFields stdArgs locals (F.ReceiveFrom msg src vc)
+    = B.Receive mtype (guardMsgArgs msg) (transMaybeSrc src) (transMaybeVC vc)
+
+    where
+
+        mtype = mtypeFromMsg msg
+
+        transMaybeSrc Nothing    = Nothing
+        transMaybeSrc (Just src) = Just $ transVar machine
+                                                   machineFields
+                                                   stdArgs
+                                                   locals
+                                                   src
+
+        transMaybeVC Nothing   = Nothing
+        transMaybeVC (Just vc) = Just $ getVCName vc
+
+
+        guardMsgArgs :: F.Msg -> [(B.ArgName, B.Field)]
+        guardMsgArgs (F.Msg _ args) = let maybes = map transArg args
+                                          noNothing = filter (/=Nothing) maybes
+                                      in  map fromJust noNothing
+           where
+               transArg :: F.MsgArg -> Maybe (B.ArgName, B.Field)
+               transArg (F.MsgArg _) = Nothing
+               transArg (F.GuardAssign typeDecl param)
+                  = let testVal  = transVar machine
+                                           machineFields
+                                           stdArgs
+                                           locals
+                                           param
+                        argName  = getTypeDeclName typeDecl
+                    in  Just (argName, testVal)
+
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+
+-- translate a state at the front end to
+-- a guard with a state at the Backend
+
+guardState :: F.MachineType -> F.State -> B.Guard
+guardState machine state = let stateName  = getStateName state
+                               bMachine   = B.AnyType machine
+                           in  B.AtState bMachine stateName
+
+
+
+
+
+
+
+
+
+
+
+
+-----------------------------------------------------------------
+-----------------------------------------------------------------
