cf0591e7b46116827402aee4d83619fbac653c87
Kyriakos Katsamaktsis <kyriakosk5@gmail.com>
2016-07-26 14:11:59 +0300

    Basics of machine functions
    We wrote the skeleton of the file, introduced comments about todos and
    implemented functions for finding and transforming sets in the machine
    fields, and for grouping the instances of the receive function by state
    and translating such groupings.

---
+++ src/GetMachineFunctions.hs
@@ -0,0 +1,169 @@
+-----------------------------------------------------------------
+----------------------- Machine functions -----------------------
+---------------- Receive functions, broadcasting ----------------
+
+module GetMachineFunctions where
+
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+
+import qualified Ast as F
+import qualified Backend.Murphi.MurphiAST as B
+import Data.Maybe -- for fromJust
+import Data.List  -- for nub
+import TransGen
+import TransMsg
+import TransGuard
+import TransResponse
+
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+
+-- BCastInfo = BCast MachineType SetName ElemType Message
+--ReceiveFunction = [ ( State, [ (Maybe Guard, [Response]) ] ) ]
+
+
+
+
+
+getMachineFunctions :: F.Ast -> B.MachineFunctions
+getMachineFunctions fAst = undefined
+
+
+-----------------------------------------------------------------
+
+singleMFunction :: [B.MsgArg] ->   -- standard msg arguments format
+                    F.Machine -> (B.MachineType, B.Sets, [B.BCastInfo],
+                                  B.ReceiveFunction, B.LocalVariables)
+                                  -- mfunction :: [( State, Guard,  Maybe State, [Response] )]
+                                  -- backend equivalent in B.ReceiveFunction is List of
+                                  -- ( State, [ (Maybe Guard, [Response])
+singleMFunction stdArgs (F.Machine _ machine _ _ fields mFunction)
+   = let
+         sets = findSets fields
+
+     in undefined
+
+
+   -- can use findLocal on [F.Response]
+
+-----------------------------------------------------------------
+
+-- write a method for finding pairs of messages and sets for which we need
+-- a broadcast procedure
+
+
+
+
+--------------------------------
+
+-- find the machine fields that are sets
+
+findSets :: [F.Field] -> [B.TypeDecl]
+findSets ( (F.Field typeDecl _ ):rest )
+   = case typeDecl of
+       F.Set _ _ -> transTypeDecl typeDecl : findSets rest
+       _       -> findSets rest
+
+--------------------------------
+
+-- Group MachineFCases i.e. tuples (State, Guard, Maybe State, Responses)
+-- by the same starting state
+-- F.MachineFCase :: (F.State, F.Guard, Maybe F.State, F.Responses)
+groupSameStart :: [F.MachineFCase] ->
+                  [(F.State, [(F.Guard, Maybe F.State, F.Responses)])]
+groupSameStart cases
+  = let
+        startSts = nub $ map ( \(startSt, guard, nextSt, resps)
+                                -> (startSt,[]) )
+                             cases
+
+        -- to apply the helper function addMany we must transform cases to
+        -- key value pairs
+        makeKeyVals = map ( \(startSt, guard, nextSt, resps)
+                              -> (startSt, (guard, nextSt, resps)) )
+                          cases
+
+    in
+        -- group MachineFCases according to state
+        addMany makeKeyVals startSts
+
+   where
+
+       -- we have a map where the values are lists.
+       -- Given a key,value pair we look up the key and add the
+       -- value to the list of this key
+       addOne :: (Eq a, Eq b) => (a,b) -> [(a,[b])] -> [(a,[b])]
+       addOne (k,v) list = let prevVal = fromJust $ lookup k list
+                               newVal  = prevVal ++ [v]
+                           in  (delete (k,prevVal) list) ++ [(k,newVal)]
+
+       -- we apply the above to a list of key,value pairs.
+       -- That is we group the values according to the keys,
+       addMany :: (Eq a,Eq b) => [(a,b)] -> [(a,[b])] -> [(a,[b])]
+       addMany [] final = final
+       addMany (first : rest) list
+         = let withFirst = addOne first list
+           in  addMany rest withFirst
+
+----------------------------------------------------------------
+
+
+-- we transform all the reactions with the same starting state
+sameStartSt :: F.MachineType -> [F.Field]  -- the machine and its fields
+            -> [B.MsgArg]                  -- standard form of msg in Murphi
+            -> B.LocalVariables
+            ------------------------
+            -> (F.State, [(F.Guard, Maybe F.State, F.Responses)])
+            -> B.Reaction
+
+sameStartSt machine machineFields stdArgs locals (fState, withoutStart)
+   = let
+         -- get the responses and the guards of each machineFCase
+         guardsResps = map singleGuard withoutStart
+         startState = transState fState
+
+     in
+        (startState, guardsResps)
+
+    where
+        singleGuard :: (F.Guard, Maybe F.State, [F.Response])
+                       -> (Maybe B.Guard, [B.Response])
+        singleGuard (fGuard, fNextSt, fResps)
+         = let
+               bGuard = Just $ transReceiveMsg machine
+                                               machineFields
+                                               stdArgs
+                                               locals
+                                               fGuard
+               -- go to next state
+               bNextSt = if fNextSt == Nothing then []
+                         else [B.ToState (B.AnyType machine)
+                                         (transState $ fromJust fNextSt)]
+
+               bResps = map (transResponse machine
+                                           machineFields
+                                           stdArgs
+                                           locals)
+                            fResps
+
+                         ++ bNextSt
+
+           in  (bGuard, bResps)
+
+----------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+-----------------------------------------------------------------
+-----------------------------------------------------------------
